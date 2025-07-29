afb73c9dae6737560be3e9791e6ac6d7d1eaa048
BaldaKranthi <159222060+BaldaKranthi@users.noreply.github.com>
2025-07-29 19:34:28 +0530

    Delete SpringBootMongoDBOrderAPI directory

--- SpringBootMongoDBOrderAPI/src/main/java/com/example/demo/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface OrderRepository extends MongoRepository<Order, String> {
-}
